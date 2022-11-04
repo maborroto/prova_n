925736d3c0c19a8acde1c0a50b8c9fcaeafcebee
Manuel <manuel.borroto@unical.it>
2022-11-04 15:09:17 +0100

    Initial commit

---
+++ src/main/java/it/unical/prova/App.java
@@ -0,0 +1,13 @@
+package it.unical.prova;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
